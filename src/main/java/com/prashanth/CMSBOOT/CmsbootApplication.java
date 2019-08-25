e7073721573a3fbefcf7456d41cb03c205811007
unknown <prashanth.pasham15@gmail.com>
2019-08-25 14:03:13 +0530

    inital commot

---
+++ src/main/java/com/prashanth/CMSBOOT/CmsbootApplication.java
@@ -0,0 +1,14 @@
+package com.prashanth.CMSBOOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CmsbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsbootApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/prashanth/CMSBOOT/CmsbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.prashanth.CMSBOOT;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CmsbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
